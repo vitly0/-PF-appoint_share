8a81dfc062a8b049b221f0a21dbd359412b06e20
EC2 Default User <ec2-user@ip-172-31-8-59.ap-northeast-1.compute.internal>
2022-11-20 16:46:17 +0000

    ユーザー検索以外レイアウト

--- app/controllers/admin/schedules_controller.rb
+++ app/controllers/admin/schedules_controller.rb
@@ -6,9 +6,7 @@ class Admin::SchedulesController < ApplicationController
   def show
     @schedule = Schedule.find(params[:id])
     @schedule_comments = @schedule.schedule_comments
-    @schedule_comment = current_end_user.schedule_comments.new
     @reports = @schedule.reports
-    @report = current_end_user.reports.new
     @attends = @schedule.attends
   end
 
